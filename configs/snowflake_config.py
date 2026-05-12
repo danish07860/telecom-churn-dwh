f22f6e6edc696ca8a972b0e21a0fd481883bdf7c
danish07860 <mddanishalauddin018@gmail.com>
2026-05-12 17:42:20 +0000

    Removed Snowflake credentials from tracking

--- configs/snowflake_config.py
+++
@@ -1,9 +0,0 @@
-SNOWFLAKE_CONFIG = {
-    "user": "danish786",
-    "password": "MonishAlauddin123",
-    "account": "yhtsvco-to35802",
-    "warehouse": "COMPUTE_WH",
-    "database": "TELECOM_DWH",
-    "schema": "STAGING",
-    "role": "ACCOUNTADMIN"
-}
